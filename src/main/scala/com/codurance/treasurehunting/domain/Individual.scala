56074ab056027a246070ca3516817f1c9748f000
Sandro Mancuso <sandromancuso@yahoo.com>
2016-06-10 00:35:31 +0100

    Individual stringRepresentation method created

--- src/main/scala/com/codurance/treasurehunting/domain/Individual.scala
+++ src/main/scala/com/codurance/treasurehunting/domain/Individual.scala
@@ -2,5 +2,8 @@ package com.codurance.treasurehunting.domain
 
 case class Individual(actions: Seq[Action.Value]) {
 
+	def stringRepresentation(): String =
+		actions.map(a => a.id.toString).mkString
+
 }
 
